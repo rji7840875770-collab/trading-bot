5aea76e8ea734abeae102c9b4f13710652d142b2
rji7840875770-collab <rji.7840875770@gmail.com>
2026-06-08 07:19:10 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,6 +6,7 @@ import ta
 import requests as req
 from SmartApi import SmartConnect
 from datetime import datetime, timedelta
+import pytz
 
 # ─── CREDENTIALS ────────────────────────────────
 API_KEY          = os.environ.get("API_KEY")
@@ -124,10 +125,11 @@ def run():
     time.sleep(10)
     while True:
         try:
-            now          = datetime.now()
-            market_open  = now.replace(hour=9,  minute=15, second=0)
-            market_close = now.replace(hour=15, minute=30, second=0)
-            if not (market_open <= now <= market_close) or now.weekday() > 4:
+            ist          = pytz.timezone('Asia/Kolkata')
+now          = datetime.now(ist)
+market_open  = now.replace(hour=9,  minute=15, second=0, microsecond=0)
+market_close = now.replace(hour=15, minute=30, second=0, microsecond=0)
+if not (market_open <= now <= market_close) or now.weekday() > 4:
                 print("Market closed. Waiting 15 minutes...")
                 time.sleep(900)
                 continue
